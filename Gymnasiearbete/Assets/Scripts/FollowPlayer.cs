6e285da34eb0fb9f040229ec419077c8dcac9d2e
Unknown <kizburb@gmail.com>
2019-11-21 20:53:47 +0100

    Fix Project

---
+++ Gymnasiearbete/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Gymnasiearbete/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called each time the game is run
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 1);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(1, 0, 0);
+        }
+    }
+}
